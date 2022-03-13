99b1b7daabc76d6a0cdd8ddb11dd871572e24a44
Derzhko <a.derzhko@yandex.ru>
2022-03-13 21:32:00 +0300

    add navbar in page

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React, {useContext} from 'react'
+import { Context } from '../index';
+import {Navbar, Container, Nav, NavLink, Button} from 'react-bootstrap'
+import { SHOP_ROUTE } from '../utils/consts';
+import {observer} from 'mobx-react-lite';
+
+const NavBar = observer(() => {
+    const {user} = useContext(Context)
+    return (
+        <Navbar bg="dark" variant="dark">
+    <Container>
+    <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
+    {user.isAuth ? 
+        <Nav className="ml-auto" style={{color: "white"}}>
+            <Button variant={"outline-light"}>Админ панель</Button>
+            <Button variant={"outline-light"}>Выйти</Button>
+        </Nav>
+        :
+        <Nav className="ml-auto" style={{color: "white"}}>
+            <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+        </Nav>
+
+    }
+    
+    </Container>
+  </Navbar>
+    )
+})
+
+export default NavBar;
